d41990eda32e0f217366b346b2f9aec9f5603031
Tom3s <tomesjerry3@gmail.com>
2024-01-16 17:55:56 +0200

    Grass Grows with tamed spiders

--- EventListener.gd
+++ EventListener.gd
@@ -6,6 +6,9 @@ var spiderScene := preload("res://Models/Spider.tscn")
 @onready var player: Node3D = %Player
 @onready var basePlane: MeshInstance3D = %BasePlane
 
+var totalSpiderCount: int = 20
+var tamedSpiderCount: int = 0
+
 # Called when the node enters the scene tree for the first time.
 const SPIDER_SPAWN_RANGE = 75
 const MIN_SPIDER_DISTANCE = 10
@@ -14,10 +17,11 @@ func _ready():
 	# 	spider.player = player
 	var spawnedSpiders := 0
 	var occupiedPositions: PackedVector3Array = PackedVector3Array()
-	while spawnedSpiders < 20:
+	while spawnedSpiders < totalSpiderCount:
 		var spider := spiderScene.instantiate()
 		spider.player = player
 		spiders.add_child(spider)
+		spider.justTamed.connect(handleSpiderTaming)
 
 		while true:
 			var newPosition = Vector3((randf() * 2 - 1) * SPIDER_SPAWN_RANGE, 0, (randf() * 2 - 1) * SPIDER_SPAWN_RANGE)
@@ -37,3 +41,9 @@ func _ready():
 		basePlane.global_position = Vector3(pos.x * player.GRID_SIZE, 0, pos.y * player.GRID_SIZE)
 		(basePlane.get_surface_override_material(0) as ShaderMaterial).set_shader_parameter("UVOffset", Vector2(pos.x / 20.0, pos.y / 20.0))
 	)
+
+func handleSpiderTaming():
+	tamedSpiderCount += 1
+	var tamedRatio = tamedSpiderCount / float(totalSpiderCount)
+
+	(basePlane.get_surface_override_material(0) as ShaderMaterial).set_shader_parameter("MixWeight", tamedRatio)
